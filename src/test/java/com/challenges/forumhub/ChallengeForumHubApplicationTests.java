cf0c3cc20d0ccfef0af6ea830293eac662757cb2
Jhoni Cunha <jhonicunha@gmail.com>
2025-08-01 16:35:48 -0300

    Commit inicial - Upload do projeto

---
+++ src/test/java/com/challenges/forumhub/ChallengeForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenges.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
